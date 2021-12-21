47ae6f0e14219fe55fdc7d249833f2cfcbe990c2
yann.courtel <yann.courtel@ext.cdiscount.com>
2021-12-21 09:24:07 +0100

    refacto : upgrade net6.0

--- csharp/TheatricalPlayersRefactoringKata/Performance.cs
+++ csharp/TheatricalPlayersRefactoringKata/Performance.cs
@@ -1,18 +1,17 @@
-namespace TheatricalPlayersRefactoringKata
+namespace TheatricalPlayersRefactoringKata;
+
+public class Performance
 {
-    public class Performance
+    private string _playID;
+    private int _audience;
+
+    public string PlayID { get => _playID; set => _playID = value; }
+    public int Audience { get => _audience; set => _audience = value; }
+
+    public Performance(string playID, int audience)
     {
-        private string _playID;
-        private int _audience;
-
-        public string PlayID { get => _playID; set => _playID = value; }
-        public int Audience { get => _audience; set => _audience = value; }
-
-        public Performance(string playID, int audience)
-        {
-            this._playID = playID;
-            this._audience = audience;
-        }
-
+        this._playID = playID;
+        this._audience = audience;
     }
+
 }
--- csharp/TheatricalPlayersRefactoringKata/Play.cs
+++ csharp/TheatricalPlayersRefactoringKata/Play.cs
@@ -1,16 +1,16 @@
-namespace TheatricalPlayersRefactoringKata
+namespace TheatricalPlayersRefactoringKata;
+
+public class Play
 {
-    public class Play
+    private string _name;
+    private string _type;
+
+    public string Name { get => _name; set => _name = value; }
+    public string Type { get => _type; set => _type = value; }
+
+    public Play(string name, string type)
     {
-        private string _name;
-        private string _type;
-
-        public string Name { get => _name; set => _name = value; }
-        public string Type { get => _type; set => _type = value; }
-
-        public Play(string name, string type) {
-            this._name = name;
-            this._type = type;
-        }
+        this._name = name;
+        this._type = type;
     }
 }
